11281d8673fe73815e68240b1f008092f2b43134
Diego Monroy F <diego.monroy.f@gmail.com>
2024-02-04 20:36:05 -0600

    Iterative solution is more efficient! TODO!

---
+++ leetcode/430-flatten-multilevel-doubly-linked-list/answer2.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/description/
+# Most efficient solution -- use iterative approach! TODO!
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
